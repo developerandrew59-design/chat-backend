0859c95d6262128288d5f2dbb6b6f5fb65b80a07
developerandrew59-design <developerandrew59@gmail.com>
2026-05-24 15:24:44 +0530

    added and tested websockets

--- routers/websocket_endpoint.py
+++ routers/websocket_endpoint.py
@@ -1,5 +1,9 @@
-from fastapi import APIRouter,WebSocket,WebSocketDisconnect
+from fastapi import APIRouter, Depends, HTTPException,WebSocket,WebSocketDisconnect,status
 from websocket_manager import manager
+from sqlalchemy.orm import Session
+from database import get_db
+from Oauth2 import verify_acess_token
+import models
 
 router=APIRouter(
     prefix="/ws",
@@ -7,13 +11,24 @@ router=APIRouter(
 )
 
 @router.websocket("/{room_id}")
-async def websocket_endpoint(websocket: WebSocket, room_id: int):
+async def websocket_endpoint(websocket: WebSocket, room_id: int,token: str,db:Session=Depends(get_db)):
+   
+
+    credentials_exception=HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                                        detail="invalid creditinals",
+                                        headers={"WWW-Authenticate":"bearer"})
+
+    token_data = verify_acess_token(token, credentials_exception)
+
     await manager.connect(websocket,room_id)
 
     try:
         while True:
             data=await websocket.receive_text()
-            
+            save_message=models.Message(message=data,room_id=room_id,account_id=token_data.id)
+            db.add(save_message)
+            db.commit()
+                        
             await manager.broadcast(data,room_id)
     except WebSocketDisconnect:
         await manager.disconnect(websocket,room_id)
