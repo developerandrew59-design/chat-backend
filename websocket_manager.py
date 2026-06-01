ae8ce0b0877b07684139821b2d1f06869ea5240c
developerandrew59-design <developerandrew59@gmail.com>
2026-06-01 15:27:18 +0530

    adding in redis to the workflow for faster memory caching

--- websocket_manager.py
+++ websocket_manager.py
@@ -1,25 +1,37 @@
+import asyncio
+import redis.asyncio as aioredis
+import json
+
 class ConnectionManager():
     def __init__(self):
-        self.active_connection={}
-    async def connect(self,websocket,room_id):
+        self.active_connections = {}
+        self.redis = aioredis.from_url("redis://redis:6379")
+
+    async def connect(self, websocket, room_id):
         await websocket.accept()
+        if room_id not in self.active_connections:
+            self.active_connections[room_id] = []
+        self.active_connections[room_id].append(websocket)
 
-        if room_id not in self.active_connection:
-            self.active_connection[room_id]=[]
+    async def disconnect(self, websocket, room_id):
+        self.active_connections[room_id].remove(websocket)
+        if len(self.active_connections[room_id]) == 0:
+            del self.active_connections[room_id]
 
-        self.active_connection[room_id].append(websocket)  
+    async def broadcast(self, message, room_id):
+        await self.redis.publish(f"room:{room_id}", message)
 
-    async def disconnect(self,websocket,room_id):
-        self.active_connection[room_id].remove(websocket)
+    async def listen(self, room_id):
+        pubsub = self.redis.pubsub()
+        await pubsub.subscribe(f"room:{room_id}")
+        async for msg in pubsub.listen():
+            if msg["type"] == "message":
+                data = msg["data"].decode("utf-8")
+                if room_id in self.active_connections:
+                    for connection in self.active_connections[room_id]:
+                        await connection.send_text(data)
 
-        if len(self.active_connection[room_id])==0:
-            del self.active_connection[room_id]   
-
-    async def broadcast(self,message,room_id):
-        for connection in self.active_connection[room_id]:
-            await connection.send_text(message)    
-
-manager = ConnectionManager()                    
+manager = ConnectionManager()                  
     
 
          
